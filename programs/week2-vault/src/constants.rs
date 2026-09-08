11cff7a1f20c9334b775b7fdd5d3a33daabb4541
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-09-08 19:13:24 +0530

    implement initialize and deposit

--- programs/week2-vault/src/constants.rs
+++ programs/week2-vault/src/constants.rs
@@ -1,10 +1,7 @@
 use anchor_lang::prelude::*;
 
 #[constant]
-pub const COUNTER_SEED: &[u8] = b"counter";
+pub const VAULT_SEED: &[u8] = b"vault";
 
 #[constant]
-pub const HELLO_WORLD_LAMPORTS: u64 = 1;
-
-#[constant]
-pub const MAX_COUNT: u64 = 10;
+pub const STATE: &[u8] = b"state";
--- programs/week2-vault/src/lib.rs
+++ programs/week2-vault/src/lib.rs
@@ -9,10 +9,25 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("6Dm3EYsvKi52MqWw8qRB3upUqHHj6w73wAvGhYiYvpAR");
+declare_id!("aNksHVU3gU1mjCPtTBsVk9S7qokAUvXfotB2jBxQQvv");
 
 #[program]
-pub mod week2_vault {
+pub mod q3_26_vault {
     use super::*;
 
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize(&ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)
+    }
+
+    // pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+    //     ctx.accounts.withdraw(amount)
+    // }
+
+    // pub fn close(ctx: Context<Close>) -> Result<()> {
+    //     ctx.accounts.close()
+    // }
 }
